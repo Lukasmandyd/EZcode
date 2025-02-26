d35335d6a3a109c2410368f07b9c6f2ae1db1eb1
Lukasmandyd <98669692+Lukasmandyd@users.noreply.github.com>
2025-02-27 01:31:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Mobile Menu Toggle
+document.querySelector('.dropbtn').addEventListener('click', function() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
